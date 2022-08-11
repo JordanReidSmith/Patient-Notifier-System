6f76c1126fbeecc309abee81ff4f9cd490fb359c
JordanReidSmith <jordanreidsmith32@gmail.com>
2022-08-11 12:09:44 +1000

    Added new get request to API test

--- Week 4/APITest.py
+++ Week 4/APITest.py
@@ -1,5 +1,8 @@
 import requests
 
-#x = requests.get("https://82jxgqb1kb.execute-api.ap-southeast-2.amazonaws.com/Initial-Testing?tableName=employees")
-x = requests.post("https://82jxgqb1kb.execute-api.ap-southeast-2.amazonaws.com/Initial-Testing?employee_id=420&first_name=Jordan&last_name=Reid_Smith&email=coolkidemail&phone_number=6942069&hire_date=2022-08-03&job_id=4&salary=69000.00&manager_id=NULL&department_id=9")
+#x = requests.get("https://82jxgqb1kb.execute-api.ap-southeast-2.amazonaws.com/Initial-Testing?tableName=regions")
+
+
+
+#x = requests.post("https://82jxgqb1kb.execute-api.ap-southeast-2.amazonaws.com/Initial-Testing?employee_id=420&first_name=Jordan&last_name=Reid_Smith&email=coolkidemail&phone_number=6942069&hire_date=2022-08-03&job_id=4&salary=69000.00&manager_id=NULL&department_id=9")
 print(str(x.text))
