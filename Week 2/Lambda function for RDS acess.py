10a93c3f4c311aed1328cdd3869eede24c9b7f4c
jreidsmith <64458710+jreidsmith@users.noreply.github.com>
2022-08-17 11:30:26 +1000

    Added AWS Secret compatibility to lambda functions
    - RDS Test function
    - GET API function
    - POST API function

--- Week 2/Lambda function for RDS acess.py
+++ Week 2/Lambda function for RDS acess.py
@@ -1,10 +1,12 @@
 import json
+import boto3
 import pymysql
 
 def lambda_handler(event, context):
+    creds = get_secret()
     # Attempt connection to MySQL server
     try:
-        conn = pymysql.connect(host="jordan-sample-lambda-database.c0ls2h2typle.ap-southeast-2.rds.amazonaws.com", user="admin", passwd="LkE205Sl2I", db="test_schema", connect_timeout=5)
+        conn = pymysql.connect(host=creds['host'], user=creds['username'], passwd=creds['password'], db="test_schema", connect_timeout=5)
     except pymysql.MySQLError as e:
         # If connection failed, report back exception and end execution
         return {
@@ -24,3 +26,26 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Success')
     }
+
+def get_secret():
+    # Secret manager reference
+    secret_name = "Jordan-RDS-Cred"
+    region_name = "ap-southeast-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    
+    # Get encrypted secret value
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    
+    # Turn stringified secret back into JSON
+    return json.loads(secret)
--- Week 4/Lambda API Post function.py
+++ Week 4/Lambda API Post function.py
@@ -1,10 +1,12 @@
 import json
 import pymysql
+import boto3
 
 def lambda_handler(event, context):
+    creds = get_secret()
     # Attempt connection to MySQL server
     try:
-        conn = pymysql.connect(host="jordan-sample-lambda-database.c0ls2h2typle.ap-southeast-2.rds.amazonaws.com", user="admin", passwd="LkE205Sl2I", db="test_schema", connect_timeout=5)
+        conn = pymysql.connect(host=creds['host'], user=creds['username'], passwd=creds['password'], db="test_schema", connect_timeout=5)
     except pymysql.MySQLError as e:
         # If connection failed, report back exception and end execution
         return {
@@ -12,6 +14,32 @@ def lambda_handler(event, context):
         'body': json.dumps(str(e))
     }
     
+    table = event['table_name']
+    
+    if table == "employees":
+        employeeInsert(event, conn)
+    elif table == "region":
+        regionInsert(event, conn)
+    
+    # Return success 
+    return {
+        'statusCode': 200,
+        'body': json.dumps("All good")
+    }
+    
+def regionInsert(event, conn):
+    regionId = event['region_id']
+    regionName = event['region_name']
+    with conn.cursor() as cur:
+        try:
+            cur.execute("INSERT INTO regions(region_id, region_name) VALUES ({0},'{1}');".format(regionId, regionName))
+        except Exception as e:
+            return str(e)
+        for row in cur:
+            results.append(row)
+    conn.commit()
+    
+def employeeInsert(event, conn):
     employee_id = event['employee_id']
     first_name = event['first_name']
     last_name = event['last_name']
@@ -33,8 +61,25 @@ def lambda_handler(event, context):
             results.append(row)
     conn.commit()
     
-    # Return success 
-    return {
-        'statusCode': 200,
-        'body': json.dumps("All good")
-    }
+def get_secret():
+    # Secret manager reference
+    secret_name = "Jordan-RDS-Cred"
+    region_name = "ap-southeast-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+    
+    # Get encrypted secret value
+    get_secret_value_response = client.get_secret_value(
+        SecretId=secret_name
+    )
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+    
+    # Turn stringified secret back into JSON
+    return json.loads(secret)
