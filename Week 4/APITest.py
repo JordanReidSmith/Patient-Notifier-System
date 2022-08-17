47c650cbd3fe2b0d2452e5ce99163ae72bdcc220
jreidsmith <64458710+jreidsmith@users.noreply.github.com>
2022-08-17 11:27:49 +1000

    Added yaml files for all API's
    Added yaml files for original AIO API, as well as the seperated GET and POST API's

--- Week 4/APITest.py
+++ Week 4/APITest.py
@@ -1,8 +1,10 @@
 import requests
 
-#x = requests.get("https://82jxgqb1kb.execute-api.ap-southeast-2.amazonaws.com/Initial-Testing?tableName=regions")
+x = requests.get("https://ha5xzicekb.execute-api.ap-southeast-2.amazonaws.com/First-Deployment?table_name=region")
 
 
+#x = requests.post("https://lv1vhxh4g9.execute-api.ap-southeast-2.amazonaws.com/First-Deployment?table_name=region&region_id=7&region_name=Mars")
 
-#x = requests.post("https://82jxgqb1kb.execute-api.ap-southeast-2.amazonaws.com/Initial-Testing?employee_id=420&first_name=Jordan&last_name=Reid_Smith&email=coolkidemail&phone_number=6942069&hire_date=2022-08-03&job_id=4&salary=69000.00&manager_id=NULL&department_id=9")
+
+#x = requests.post("https://lv1vhxh4g9.execute-api.ap-southeast-2.amazonaws.com/First-Deployment?table_name=employees&employee_id=420&first_name=Jordan&last_name=Reid_Smith&email=coolkidemail&phone_number=6942069&hire_date=2022-08-03&job_id=4&salary=69000.00&manager_id=NULL&department_id=9")
 print(str(x.text))
