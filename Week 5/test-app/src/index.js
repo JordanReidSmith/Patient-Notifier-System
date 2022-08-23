bd79bb96f33eacceeb515421b0c64b56a8b7f151
JordanReidSmith <jordanreidsmith32@gmail.com>
2022-08-23 14:17:50 +1000

    Week 5 folder added, React work started
    created a folder for work completed in week 5, beginning of project web page created, react objects for notification adding menu.

---
+++ Week 5/test-app/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+class PNSTitle extends React.Component {
+  render() {
+    return (
+      <div>
+      <h1>Patient Notifier System</h1>
+      </div>
+    )
+  }
+}
+
+class AddNotification extends React.Component {
+  render() {
+    return (
+      <center>
+        <div>
+          <h2 style={{display: 'inline-block', marginLeft: '2.3em'}}>Notifier Title:</h2>
+          <input type="text" id="fname" name="fname" style={{marginLeft: '1em'}}></input>
+        </div>
+        <div>
+          <h2 style={{display: 'inline-block', marginLeft: '-1em'}}>Notifier Description:</h2>
+          <input type="text" id="fname" name="fname" style={{marginLeft: '1em'}}></input>
+        </div>
+        <button>Add</button>
+      </center> 
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <center>
+      <PNSTitle />
+    </center>
+    <AddNotification />
+  </div>
+);
+
+reportWebVitals();
